82f466fea732970c60f037cf6219973da5f83c91
iwasaki03 <iwsonomi331@gmail.com>
2023-12-08 14:29:07 +0900

    結合テストの実装

--- Gemfile
+++ Gemfile
@@ -76,6 +76,3 @@ end
 
 gem 'devise'
 gem 'pry-rails'
-
-gem 'mini_magick'
-gem 'image_processing', '~> 1.2'
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+<<<<<<< HEAD
   has_one_attached :image
   validates :content, presence: true, unless: :was_attached?
 
@@ -8,4 +9,7 @@ class Message < ApplicationRecord
     self.image.attached?
   end
 
+=======
+  validates :content, presence: true
+>>>>>>> parent of 63258b6 (Merge pull request #5 from iwasaki03/画像投稿機能の実装)
 end
